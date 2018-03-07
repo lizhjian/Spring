f53566b55987637d3db02e13ee1a68fb949e8be1
lizhijian <907257608@qq.com>
2018-03-07 23:34:06 +0800

    增加byName ByType 自动装配

---
+++ src/com/wuxin/autowire/Car.java
@@ -0,0 +1,30 @@
+package com.wuxin.autowire;
+
+public class Car {
+   private String brand;
+   private double price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
