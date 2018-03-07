5683dc53a935251dbd6a0860a05f1b8b0e84b773
lizhijian <907257608@qq.com>
2018-03-07 23:52:46 +0800

    增加Bean的继承

---
+++ src/com/wuxin/realtion/BeanRelation.java
@@ -0,0 +1,14 @@
+package com.wuxin.realtion;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanRelation {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-relation.xml");
+    //    Address address = (Address)ctx.getBean("address");
+        Address address2 = (Address)ctx.getBean("address2");
+     //   System.out.println(address.toString());
+        System.out.println(address2.toString());
+    }
+}
