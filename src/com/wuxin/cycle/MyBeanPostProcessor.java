3053a23826043787204d2ab5124d65b563952095
lizhijian <907257608@qq.com>
2018-03-08 08:21:24 +0800

    增加Bean前置及后置处理

---
+++ src/com/wuxin/cycle/MyBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package com.wuxin.cycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        System.out.println("前置处理器");
+        return o;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        System.out.println("后置处理器");
+        return o;
+    }
+}
